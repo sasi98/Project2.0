5d79e2c73d714cd9deb3c332d911943bbaa3a080
esidfan <sidian.fang@ericsson.com>
2014-05-22 11:32:32 +0100

    Add static class Value, add combobox for datatype choose

---
+++ src/valueset/Value.java
@@ -0,0 +1,11 @@
+package valueset;
+
+public class Value {
+
+    public class ComboBox {
+        public final static String SOLAPADOS = "Solapados";
+        public final static String NOSOLAPADOS = "No Solapados";
+        public final static String ALEATORIOS = "Aleatorios";
+        public final static String MANUAL = "Manual";
+    }
+}
